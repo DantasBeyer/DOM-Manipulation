cc2bea3ad319cd32dcd6da378c7f8b46b4b1d91b
Dantas Beyer <dantasbeyer@gotmail.com>
2023-04-20 13:19:57 +0200

    updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //DOM Manipoulation - Exercice 2 (Styling an Element)
 
-const title = document.document.querySelector('#main-heading')
+const title =  document.querySelector('#main-heading')
 //inline document.Styling
 title.style.color = 'green'; 
 
@@ -24,4 +24,27 @@ const newLi = document.createElement('li');
 
 
 // adding Elements 
-document.body.appendChild(li)
+ul.append(newLi)
+
+//modifying the text
+const firstListIten = document.querySelector('.list-items');
+console.log(firstListIten);
+
+newLi.innerText = 'X-Man';
+
+//Modifying Attributes & Classes
+
+newLi.setAttribute('id', 'main-heading');
+newLi.removeAttribute('id');
+
+const title2 = document.querySelector('#main-heading')
+console.log(title2.getAttribute('id'));
+
+newLi.classList.add('list-items')
+/* newLi.classList.remove('list-items') */
+
+console.log(newLi.classList.contains('list-items')); // true
+
+ 
+// Remove Elements
+/* newLi.remove(); */
