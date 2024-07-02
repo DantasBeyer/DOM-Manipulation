871128b5b2311e0cdaace66d343feed281260de8
DantasBeyer <dantasbeyer@hotmail.com>
2024-07-02 11:36:47 +0200

    Rolf aufgaben added

---
+++ JS aufgaben/js/functions.js
@@ -0,0 +1,9 @@
+/*
+ * textare ist auf 2 Weisen auszulesen
+ */
+function checkInputs() {
+  console.log(document.querySelector("#nu").value);
+  console.log(document.querySelector("#text").value);
+  console.log(document.querySelector("#auto").value);
+  return 42;
+}
